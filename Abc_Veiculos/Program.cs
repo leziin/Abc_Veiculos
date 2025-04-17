4bd514e0675479acf6dafee2d1a6045656c9ccc5
leandroJader <ldandro>
2025-04-17 19:55:22 -0300

    Add project files.

---
+++ Abc_Veiculos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Abc_Veiculos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
